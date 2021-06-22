3ca0d8c3f95612c97dbefb0f859a76a358ba16e4
gekiro <gericofrancisco@gmail.com>
2021-06-22 13:05:19 +0800

    Added UI design for Login, Registration, and Forgot Password page.

--- app/src/main/java/com/example/slambook/OnLoadScreen.java
+++ app/src/main/java/com/example/slambook/OnLoadScreen.java
@@ -2,6 +2,7 @@ package com.example.slambook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class OnLoadScreen extends AppCompatActivity {
