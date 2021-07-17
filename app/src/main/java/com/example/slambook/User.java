32c6367c56d83b511d4ef6f8a0247da3e6635b71
gekiro <gericofrancisco@gmail.com>
2021-07-17 16:00:01 +0800

    forgot pw

---
+++ app/src/main/java/com/example/slambook/User.java
@@ -0,0 +1,45 @@
+package com.example.slambook;
+
+public class User {
+    public String username, password, fullname, question, answer;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
